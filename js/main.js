7c21ee527072ba76f7de272d3c7ef6956b0a6564
dennyhappysomuch <den_happy@bk.ru>
2024-12-26 00:48:21 +0700

    Добавит валидацию

--- js/main.js
+++ js/main.js
@@ -2,5 +2,8 @@ import './utils.js';
 import './pictures.js';
 import {dataPhoto} from './data.js';
 import { randerGallery } from './modal-picture.js';
+import './form.js';
 
 randerGallery(dataPhoto);
+
+
