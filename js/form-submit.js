796405e30064bdc2198a1946e7fd1a72925e32ff
dennyhappysomuch <den_happy@bk.ru>
2024-12-27 02:07:59 +0700

    Фильтры. Рефакторинг

--- js/form-submit.js
+++ js/form-submit.js
@@ -1,9 +1,9 @@
-import { SUBMIT_TEXT, FILTERS } from './init.js';
+import { SUBMIT_TEXT } from './init.js';
 import { sendData } from './api.js';
 import { closeUploadForm } from './form.js';
 import { showPopup, showErrorMessage } from './popup.js';
 import { isValid } from './form-validation.js';
-import { setFilterActive, debounceRenderGallery } from './gallery.js';
+import { resetFilterOnDefault } from './gallery.js';
 
 const uploadFormElement = document.querySelector('.img-upload__form');
 const submitButtonElement = uploadFormElement.querySelector('.img-upload__submit');
@@ -26,8 +26,9 @@ const onFormSubmit = (evt) => {
       .then(() => {
         closeUploadForm();
         showPopup('success');
-        debounceRenderGallery();
-        setFilterActive(FILTERS.DEFAULT);
+
+        resetFilterOnDefault();
+
       })
       .catch(
         (err) => {
